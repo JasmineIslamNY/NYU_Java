09f9e6e47bbf753233d116ed523df211ba0b1a43
JasmineIslamNY <jasmine@tek-genie.com>
2015-06-15 23:00:32 -0400

    20150615_2300
    Homework for Day 1

--- HW2.java
+++ HW2.java
@@ -1,34 +1,49 @@
 public class HW2 {						
 
-        public static void main(String[ ] args) {							double miles_yards = 1760.0;
-		double yards_feed = 3.0f;
-		float feet_inches = 12.0f;
-		float miles_kilometers = 1760.0f;
-		float kilometers_meters = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-		float miles_yards = 1760.0f;
-
-double  miles_yards = 1760.0
-double  yards_feet = 3.0
-double  feet_inches = 12.0
-double  miles_kilometers = 1.609344
-  kilometers_meters = 1000.0
-  meters_centimeters = 100.0
-  centimeters_millimeters = 10.0
-
-  gallons_quarts = 4.0
-  quarts_pints = 2.0
-  pints_fluid ounces = 16.0
-  gallons_litters = 3.78541
-  litters_milliliters = 1000.0           
-
+        public static void main(String[ ] args) {
 		
+		// Name: Jasmine Islam Course: INFO1-CE9226 Summer 2015
+		
+		double mile = 1.0;
+		double yards = mile*1760.0;
+		double feet = yards*3.0;
+		double inches = feet*12.0;
+		double kilometers = mile*1.609344;
+		double meters = kilometers*1000.0;
+		double centimeters = meters*100.0;
+		double millimeters = centimeters*10.0;
+
+		double gallon = 1.0;
+		double quarts = gallon*4.0;
+		double pints = quarts*2.0;
+		double ounces = pints*16.0;
+		double liters = gallon*3.78541;
+		double milliliters = liters*1000.0;
+
+		System.out.println("");		
+		System.out.println("Jasmine Islam\'s Homework");
+		System.out.println("");
+		System.out.println("\t \t One mile is \t One Half mile is");
+		System.out.println("\t \t =========== \t ================");
+		System.out.println("inches \t \t " + inches + "\t " + inches*0.5);
+		System.out.println("feet \t \t " + feet + "\t \t " + feet*0.5);
+		System.out.println("yards \t \t " + yards + "\t \t " + yards*0.5);
+		System.out.println("mile \t \t " + mile + "\t \t " + mile*0.5);
+		System.out.println("millimeters \t " + millimeters + "\t " + millimeters*0.5);
+		System.out.println("centimeters \t " + centimeters + "\t " + centimeters*0.5);
+		System.out.println("meters \t \t " + meters + "\t " + meters*0.5);
+		System.out.println("kilometers \t " + kilometers + "\t " + kilometers*0.5);
+
+		System.out.println("");
+		System.out.println("\t \t One gallon is \t \t One Half gallon is");
+		System.out.println("\t \t ============= \t \t ==================");
+		System.out.println("ounces \t \t " + ounces + "\t \t \t " + ounces*0.5);
+		System.out.println("pints \t \t " + pints + "\t \t \t " + pints*0.5);
+		System.out.println("quarts \t \t " + quarts + "\t \t \t " + quarts*0.5);
+		System.out.println("gallon \t \t " + gallon + "\t \t \t " + gallon*0.5);
+		System.out.println("milliliters \t " + milliliters + "\t " + milliliters*0.5);
+		System.out.println("liters \t \t " + liters + "\t \t " + liters*0.5);	
+		System.out.println("");	
        }						
 }						
 
