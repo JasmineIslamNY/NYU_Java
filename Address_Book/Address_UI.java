9cec833c1e033475e2f2142f636762ad57b908b6
JasmineIslamNY <jasmine@tek-genie.com>
2015-06-23 01:34:40 -0400

    20150623_134
    Final final hopefully

--- Address_Book/Address_UI.java
+++ Address_Book/Address_UI.java
@@ -19,24 +19,19 @@ public class Address_UI
 
 		try 
 		{
-			InputStreamReader	kbd		= new InputStreamReader(System.in);	 
-			BufferedReader		kbdBuf		= new BufferedReader(kbd);		  	         
+			InputStreamReader	kbd = new InputStreamReader(System.in);	 
+			BufferedReader kbdBuf = new BufferedReader(kbd);		  	         
 
 	      		while(true)
 	      		{
 				System.out.println("Search the address book!");
-				System.out.println("Enter whole or partial last name please: (enter to cancel) ");
+				System.out.println
+					("Enter whole or partial last name please: (enter to cancel) ");
 
 				String input = kbdBuf.readLine();				
 
 				if (input.equals(""))						
-		      			System.exit(0);	
-									
-				//Address lastName = new Address(input);
-				//System.out.print(lastName);
-				//screenBuf.println("You entered: " + input);			
-				//screenBuf.flush( );	
-
+		      			System.exit(0);
 													
 				Address matchedRecords = new Address(input);
 
@@ -44,15 +39,18 @@ public class Address_UI
 				{
 					if (record[0] == null)
 					{
-						System.out.println("*****************************************");
+						System.out.println
+							("************************************");
 						System.out.println("No match found");
-						System.out.println("*****************************************");
+						System.out.println
+							("************************************");
 						System.out.println("");	
 						break;
 					}
-					System.out.println("*********************************************************************");	
+					System.out.println("*************************************");	
 					System.out.print("Name: "+record[1]+" "+record[0]+"\n");
-					System.out.print("Address: "+record[2]+", "+record[3]+", "+record[4]+" "+record[5]+"\n");
+					System.out.print("Address: "+record[2]+", "+
+						record[3]+", "+record[4]+" "+record[5]+"\n");
 					System.out.print("Phone Number: "+record[6]+"\n");
 					System.out.println("");	
 				} 
